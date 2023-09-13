7eeeea88f5a84a45ea4971781273b8002fc3152a
jademisato <87796782+jademisato@users.noreply.github.com>
2023-09-13 15:16:59 +0900

    Show tags.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import nltk
-f = open ("tyger.txt", "r",)
+
+f = open ("tyger.txt", "r", encoding="utf8")
 data = f.read()
 tokens = nltk.word_tokenize(data)
-print(tokens)
+tags = nltk.pos_tag(tokens)
+print(tags)
