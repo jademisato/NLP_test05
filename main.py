3b8a0aa9b8dee82b40cc2fb70dd75d7304c9f410
jademisato <87796782+jademisato@users.noreply.github.com>
2023-09-14 11:28:02 +0900

    Extract words with NNP tags

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import nltk
 
+# Show tags for all words (全ての単語のタグを表示する)
 f = open ("tyger.txt", "r", encoding="utf8")
 data = f.read()
 tokens = nltk.word_tokenize(data)
 tags = nltk.pos_tag(tokens)
-print(tags)
+
+# Extract words with NNP tags (NNPタグを持つ単語を抽出する)
+text_word = [word for word, pos in tags if pos == 'NNP']
+print(text_word)
